793089248335b26feebc8e8e98c7900801f3e6e9
Naresh Kumar <naresh22255@gmail.com>
2021-01-24 09:55:54 +0100

    Inprogress

---
+++ Scroll Animation/script.js
@@ -0,0 +1,9 @@
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkboxes);
+
+checkboxes();
+
+function checkboxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+}
