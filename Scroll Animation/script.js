8e38027a10fa0432c7f756df99192c8d488aab42
Naresh Kumar <naresh22255@gmail.com>
2021-01-27 09:39:48 +0100

    Completed the Project

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -6,4 +6,14 @@ checkboxes();
 
 function checkboxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
 }
