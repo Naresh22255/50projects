96364face6d97f5fff1037018b0f6f5cff8f36ac
Naresh Kumar <naresh22255@gmail.com>
2021-02-25 01:01:04 +0100

    Inprogress

--- Random Choice Picker/script.js
+++ Random Choice Picker/script.js
@@ -2,3 +2,16 @@ const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
 
 textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+const createTags = (input) => {
+  //   console.log(input);
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  //   console.log(tags);
+};
