3fc5747d313bda52754704c58978fae4edbaf3c0
Naresh Kumar <naresh22255@gmail.com>
2021-10-16 15:02:31 +0200

    Completed

--- Random Choice Picker/script.js
+++ Random Choice Picker/script.js
@@ -5,6 +5,13 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = " ";
+    }, 10);
+    randomSelect();
+  }
 });
 
 const createTags = (input) => {
@@ -13,5 +20,51 @@ const createTags = (input) => {
     .split(",")
     .filter((tag) => tag.trim() !== "")
     .map((tag) => tag.trim());
-  //   console.log(tags);
+  // console.log(tags);
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
 };
+
+function randomSelect() {
+  const times = 25;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unhighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unhighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
